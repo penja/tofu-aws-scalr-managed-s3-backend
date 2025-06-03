894c3f21d2bd6687ccc72be6aefdaec29df4130e
Andrii Penchuk <penja7@gmail.com>
2025-06-03 20:38:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "random_string" "names_suffix" {
   count   = var.bucket_name == null || var.dynamodb_table_name == null ? 1 : 0
-  length  = 40
+  length  = 45
   special = false
   upper   = false
 }
